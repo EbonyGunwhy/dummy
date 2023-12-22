4fae461d42ccfb2ef1b01968191e2bf134649a84
EbonyGunwhy <e.gunwhy@sheffield.ac.uk>
2023-12-22 21:47:08 +0000

    build: modify release to run on tag trigger

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, EbonyGunwhy'
 author = 'EbonyGunwhy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = "0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
