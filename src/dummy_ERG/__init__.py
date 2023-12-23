5754e9123fcdd81126701143fd0607d10996e73f
EbonyGunwhy <e.gunwhy@sheffield.ac.uk>
2023-12-23 11:13:07 +0000

    bump version 3.5.0 -> 3.5.1

--- src/dummy_ERG/__init__.py
+++ src/dummy_ERG/__init__.py
@@ -1,3 +1,3 @@
 # dummy_ERG/__init__.py
 
-__version__ = "3.5.0"
+__version__ = "3.5.1"
--- tests/test_dummy_ERG.py
+++ tests/test_dummy_ERG.py
@@ -3,4 +3,4 @@
 from dummy_ERG import __version__
 
 def test_version():
-    assert __version__ == "3.5.0"
+    assert __version__ == "3.5.1"
